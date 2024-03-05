1c9b76b7742102f9b9f2c76debbde2674f4836d5
JackobPunch <jakubcios99@gmail.com>
2024-03-05 20:08:31 +0100

    Sololearn

---
+++ 2_Sololearn/Templates/Challenge1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Div
+{
+public:
+    Div(T x, T y)
+    {
+        cout << x / y << endl;
+    }
+};
+template <>
+class Div<string>
+{
+public:
+    Div(string x, string y)
+    {
+        cout << x.size() / y.size() << endl;
+    }
+};
+
+// your code goes here
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    int x, y;
+    cin >> x >> y;
+
+    Div<string> d2(a, b);
+    Div<int> d1(x, y);
+}
